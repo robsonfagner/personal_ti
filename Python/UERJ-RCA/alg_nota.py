2cee8797a1d11d4b5bbf583004e137c2f090c789
robsonfagner <rbs.fagner@gmail.com>
2026-08-02 19:45:04 -0400

    Exercicios de RCA prof Marina

---
+++ Python/UERJ-RCA/alg_nota.py
@@ -0,0 +1,14 @@
+
+
+p1 = int(input("Por favor digite a sua primeira nota: "))
+
+p2 = int(input("Por favor digite a sua segunda nota nota: "))
+
+media = (p1+p2)/2
+
+if media >= 7:
+    print(f"A sua média foi de : {media} e você está aprovado")
+else:
+    print(f"Você está reprovado porque sua média foi de {media}, estude mais da próxima vez")
+
+8
