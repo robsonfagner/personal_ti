7212c0ab530bccfd5b7e4632b5deadf2f163a19a
robsonfagner <rbs.fagner@gmail.com>
2026-08-08 18:57:40 -0300

    Aula de funções

--- TMY/Python/dia_06/02_juroscom.py
+++ TMY/Python/dia_06/02_juroscom.py
@@ -2,9 +2,23 @@
 # %%
 
 def juros_compostos(aporte, taxa, anos):
+    ''' 
+    
+    Juros compostos servem para me encher o saco
+     
+    Aporte:Numero inteiro que represente um valor em Reais.
+
+    Anos:Tempo em anos da função
+
+    Taxa:Taxa mensal ou ano em porcentagem.
+      
+    '''
     return aporte * (1+taxa) ** anos
 
 # %%
 juros_compostos(taxa=0.13,aporte=1000, anos=4)
 
+#%%
+
+# Parei no 28:54
 
